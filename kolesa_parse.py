a624c3587470d08c09f6624614224705e88034ed
Dmitriy <dmitriy.n.kim@gmail.com>
2020-06-23 08:58:07 +0000

    Changes for krisha

--- kolesa_parse.py
+++ kolesa_parse.py
@@ -4,12 +4,13 @@ from urllib.parse import urljoin
 import time
 import os
 
-class bothandler:
-    
+
+class BotHandler:
+
     def __init__(self, token):
         self.token = token
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
-    
+
     def get_updates(self, timeout=30):
         method = 'getupdates'
         params = {'timeout': timeout}
@@ -17,23 +18,23 @@ class bothandler:
         print(resp)
         result_json = resp.json()['result']
         return result_json
-    
+
     def send_message(self, chat_id, text):
         params = {'chat_id': chat_id, 'text': text}
         method = 'sendmessage'
         resp = requests.post(self.api_url + method, params)
         return resp
-    
+
     def get_last_update(self):
         get_result = self.get_updates()
-        
+
         if len(get_result) > 0:
             last_update = get_result[-1]
         else:
             last_update = get_result[len(get_result)]
-        
+
         return last_update
-    
+
     def get_all_chat_id(self):
         get_result = self.get_updates()
         chat_ids = []
@@ -43,10 +44,11 @@ class bothandler:
         return list(set(chat_ids))
 
 
-token = "709320970:AAGYB30J43aymiYu2VTCBa9uKHCJsfXNLSc"
+token = "1190799005:AAHKm7-K7ZxSYMhykptRwLcKRr-nLgFeisM"
 
-greet_bot = bothandler(token)
-greetings = ('472717315', '671788131')
+greet_bot = BotHandler(token)
+greetings = ['133929927']
+
 
 def get_html(url):
     response = requests.get(url)
@@ -54,19 +56,25 @@ def get_html(url):
 
 
 def get_all_autos(urls):
-    div_names = ['row vw-item list-item a-elem',\
-            'row vw-item list-item blue a-elem',\
-            'row vw-item list-item yellow a-elem']
+    div_names = ['a-card a-storage-live ddl_product ddl_product_link not-colored is-visible',
+                 'a-card a-storage-live ddl_product ddl_product_link not-colored is-visible is-urgent',
+                 'a-card a-storage-live ddl_product ddl_product_link is-colored paid-color-light-red is-visible',
+                 'a-card a-storage-live ddl_product ddl_product_link is-colored paid-color-light-red is-visible is-urgent',
+                 'a-card a-storage-live ddl_product ddl_product_link is-colored paid-color-light-yellow is-visible',
+                 'a-card a-storage-live ddl_product ddl_product_link is-colored paid-color-light-yellow is-visible is-urgent',
+                 'a-card a-storage-live ddl_product ddl_product_link is-colored paid-color-light-green is-visible',
+                 'a-card a-storage-live ddl_product ddl_product_link is-colored paid-color-light-green is-visible is-urgent']
     all_divs = []
     for url in urls:
-        if url =='':
+        if url == '':
             continue
         html = get_html(url)
         soup = BeautifulSoup(html, 'lxml')
-        divs = soup.find_all("div", class_ = div_names)
+        divs = soup.find_all("div", class_=div_names)
         all_divs.extend(divs)
 
     return all_divs
+
 
 def check(filename):
     if not os.path.exists(filename):
@@ -77,39 +85,43 @@ def check(filename):
 def main():
     try:
         check('urls.xml')
-        check('passed_autos.xml')
+        check('passed_flats.xml')
         tt = time.time()
         with open('urls.xml', 'r') as file:
             urls = file.read().split('\n')
-        with open('passed_autos.xml', 'r') as file:
+        with open('passed_flats.xml', 'r') as file:
             passed_autos = file.read().split('\n')
-        print(time.time()-tt, "Time spent reading the file")
-        while (1):
+        print(time.time() - tt, "Time spent reading the file")
+        while True:
+            time.sleep(300)
             t = time.time()
             all_autos = get_all_autos(urls)
-            #print(len(all_autos))
+            # print(len(all_autos))
             ids_dict = {}
             for div in all_autos:
                 if div['id'] in passed_autos:
                     continue
                 else:
-                    with open('passed_autos.xml', 'a') as file:
-                        file.write(div['id']+'\n')
+                    with open('passed_flats.xml', 'a') as file:
+                        file.write(div['id'] + '\n')
                     passed_autos.append(div['id'])
-                    href = urljoin('https://kolesa.kz', div.a['href'])
-                    #last_region = div['a-info-side col-right-list']
-                    price = div.contents[1].contents[0].contents[1].text.lstrip()
-                    name = div.contents[1].contents[0].contents[0].a.text
-                    info = div.contents[1].contents[2].contents[0].text.lstrip()
-                    date = div.contents[1].contents[3].contents[0].contents[1].text.lstrip()
-                    ids_dict.update({div['id']:[str(href), '  '+str(price), str(name), str(info), str(date)]})
+                    href = urljoin('https://krisha.kz', div.a['href'])
+                    # last_region = div['a-info-side col-right-list']
+                    price = div.contents[1].contents[3].contents[1].contents[1].contents[3].text.lstrip()
+                    name = div.contents[1].contents[3].contents[1].contents[1].contents[1].contents[1].text.lstrip()
+                    address = div.contents[1].contents[3].contents[1].contents[3].contents[1].text.strip()
+                    info = div.contents[1].contents[3].contents[1].contents[3].contents[3].text.strip()
+                    date = div.contents[1].contents[3].contents[3].contents[3].contents[1].contents[3].text.strip()
+                    ids_dict.update({div['id']: [str(href), '  ' + str(price), str(name), str(address), str(info),
+                                                 str(date)]})
                     # vkapi.messages.send(domain='id204817379', message=ids_dict[div['id']], v=3)
                     greet_bot.send_message(greetings[0], ids_dict[div['id']])
-                    greet_bot.send_message(greetings[1], ids_dict[div['id']])
+                    # greet_bot.send_message(greetings[1], ids_dict[div['id']])
 
-            print(time.time()-t, 'sec', "One iteration time")
+            print(time.time() - t, 'sec', "One iteration time")
     except Exception as e:
         print(e)
+
 
 if __name__ == '__main__':
     main()
